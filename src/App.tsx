2628a20a4cae609f2ae67aed67eaf9acab5d453e
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-18 17:17:43 +0100

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
-import Main from "./Main";
+import Main from "./main";
 
 export default function App() {
   return (
